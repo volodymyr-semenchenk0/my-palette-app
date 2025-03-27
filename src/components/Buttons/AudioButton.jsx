4128efff713b8218f85c5bc6edc1c140eace35d4
volodymyr-semenchenko <semenchenko.volodymyr1118@vu.cdu.edu.ua>
2025-03-27 20:34:27 +0200

    update audio context

--- src/components/Buttons/AudioButton.jsx
+++ src/components/Buttons/AudioButton.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {useAudio} from "../../contexts/AudioContext.jsx";
 
 const AudioButton = () => {
--- src/hooks/useClipboardNotification.js
+++ src/hooks/useClipboardNotification.js
@@ -1,38 +1,38 @@
 import {useState} from "react";
-import {useAudio} from "../contexts/AudioContext.jsx";
 import clickSound from "../assets/src_notify.mp3";
+import {useAudio} from "../contexts/AudioContext.jsx";
 
 export default function useClipboardNotification() {
-    const [open, setOpen] = useState(false);
-    const [copiedColor, setCopiedColor] = useState("");
-    const { audioEnabled } = useAudio();
+  const [open, setOpen] = useState(false);
+  const [copiedColor, setCopiedColor] = useState("");
+  const {audioEnabled} = useAudio();
 
-    const handleOpen = () => {
-        setOpen(true);
+  const handleOpen = () => {
+    setOpen(true);
 
-        if (audioEnabled) {
-            new Audio(clickSound).play();
-        }
+    if (audioEnabled) {
+      new Audio(clickSound).play();
     }
+  }
 
-    const handleClose = () => {
-        setOpen(false);
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+  async function copyColor(color,) {
+    try {
+      await navigator.clipboard.writeText(color);
+      setCopiedColor(color);
+      handleOpen();
+
+      setTimeout(() => {
+        handleClose()
+      }, 760);
+
+    } catch (error) {
+      console.error(error.message);
     }
+  }
 
-    async function copyColor(color,) {
-        try {
-            await navigator.clipboard.writeText(color);
-            setCopiedColor(color);
-            handleOpen();
-
-            setTimeout(() => {
-                handleClose()
-            }, 760);
-
-        } catch (error) {
-            console.error(error.message);
-        }
-    }
-
-    return [open, copiedColor, copyColor];
+  return [open, copiedColor, copyColor];
 }
